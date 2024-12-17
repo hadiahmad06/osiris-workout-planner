4eb132c9cdcebc966c763ad35262abdd9d24411c
hadiahmad06 <162732770+hadiahmad06@users.noreply.github.com>
2024-12-17 15:21:27 -0600

    Initial Commit

---
+++ Osiris/OsirisApp.swift
@@ -0,0 +1,17 @@
+//
+//  OsirisApp.swift
+//  Osiris
+//
+//  Created by Hadi Ahmad on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct OsirisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OsirisTests/OsirisTests.swift
@@ -0,0 +1,17 @@
+//
+//  OsirisTests.swift
+//  OsirisTests
+//
+//  Created by Hadi Ahmad on 12/17/24.
+//
+
+import Testing
+@testable import Osiris
+
+struct OsirisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
